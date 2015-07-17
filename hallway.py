91cdea6cd1f96d4c29198c2fc3bf7c9d049ec531
Erik Zawadzki <epz@cs.cmu.edu>
2015-07-17 18:38:42 -0400

    Added file for the hallway problem

---
+++ hallway.py
@@ -0,0 +1,29 @@
+import numpy as np
+from mdp import MDP
+
+def generate_hallway_mdp(num_states,**kwargs):
+    """ Generates an MDP based on the hallway problem
+    Arguments: num_states (int)
+    Returns: MDP object
+    """
+    # Wheel_slip: probability of going nowhere
+    wheel_slip = kwargs.get('wheel_slipe',0.1)
+
+    # Actions: move left or right
+    Actions = [-1,1]
+
+    # Form the cost
+    target = num_states / 2
+    cost = np.ones(num_states)
+    cost[target] = 0
+    Costs = [cost,cost]
+
+    # Form the transition matrices
+    Transitions = []
+    for i in Actions:
+        P = wheel_slip*scipy.sparse.eye(num_states) \
+           + (1 - wheel_slip)*scipy.sparse.diags(np.ones(num_states-1),i)
+        Transitions.append(P)
+
+    return MDP(Transitions,Costs,Actions)
+    
