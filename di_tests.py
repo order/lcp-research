fbbd2414b5e8ac249b9db4abf03c54b8e240e378
Erik Zawadzki <epz@cs.cmu.edu>
2015-10-16 13:22:46 -0700

    Cleaning up di_tests

--- di_tests.py
+++ di_tests.py
@@ -24,7 +24,8 @@ import time
 
 def map_back_test():
     """
-    Check to make sure that all nodes map back to themselves when run through the
+    Check to make sure that all nodes map back to themselves when 
+    run through the
     node -> state -> node machinery
     """
     
@@ -56,7 +57,8 @@ def plot_remap(discretizer,action,sink_states):
 
     N = states.shape[0]
     for i in xrange(N):
-        plt.plot([states[i,0],next_states[i,0]],[states[i,1],next_states[i,1]],'-b',lw=2)
+        plt.plot([states[i,0],next_states[i,0]],\
+                 [states[i,1],next_states[i,1]],'-b',lw=2)
     
     T = discretizer.states_to_transition_matrix(next_states)
     (Nodes,States) = T.nonzero()
@@ -72,7 +74,8 @@ def plot_remap(discretizer,action,sink_states):
             x = sink_states[node_id - N,0]
             y = sink_states[node_id - N,1]    
         
-        plt.plot([x,next_states[state_id,0]],[y,next_states[state_id,1]],'-r',alpha=T[node_id,state_id])
+        plt.plot([x,next_states[state_id,0]],\
+                 [y,next_states[state_id,1]],'-r',alpha=T[node_id,state_id])
     plt.show()
     
 def plot_costs(discretizer,action):
@@ -110,19 +113,14 @@ def plot_value_function(discretizer,value_fun_eval,**kwargs):
     (v_lo,v_hi) = boundary[1]
     
     grid = kwargs.get('grid_size',51)
-    [x_mesh,v_mesh] = np.meshgrid(np.linspace(x_lo,x_hi,grid), np.linspace(v_lo,v_hi,grid),indexing='ij')
+    [x_mesh,v_mesh] = np.meshgrid(np.linspace(x_lo,x_hi,grid),\
+                                  np.linspace(v_lo,v_hi,grid),indexing='ij')
     Pts = np.column_stack([x_mesh.flatten(),v_mesh.flatten()])
     
     vals = value_fun_eval.evaluate(Pts)
     ValueImg = np.reshape(vals,(grid,grid))
     
-    three_d = False
-    if three_d:
-        fig = plt.figure()
-        ax = fig.gca(projection='3d')
-        ax.plot_surface(x_mesh,v_mesh,ValueImg,rstride=1, cstride=1, cmap=cm.jet, linewidth=0, antialiased=False)
-    else:
-        plt.pcolor(x_mesh,v_mesh,ValueImg)
+    plt.pcolor(x_mesh,v_mesh,ValueImg)
         
     plt.xlabel('Position')
     plt.ylabel('Velocity')
@@ -135,7 +133,8 @@ def plot_policy(discretizer,policy,**kwargs):
     (v_lo,v_hi) = boundary[1]
     
     grid = kwargs.get('grid_size',101)
-    [x_mesh,y_mesh] = np.meshgrid(np.linspace(x_lo,x_hi,grid), np.linspace(v_lo,v_hi,grid),indexing='ij')
+    [x_mesh,y_mesh] = np.meshgrid(np.linspace(x_lo,x_hi,grid),\
+                                  np.linspace(v_lo,v_hi,grid),indexing='ij')
     Pts = np.column_stack([x_mesh.flatten(),y_mesh.flatten()])
     
     
@@ -153,18 +152,13 @@ def plot_advantage(discretizer,value_fun_eval,action1,action2):
     next_states1 = discretizer.physics.remap(states,action=action1)
     next_states2 = discretizer.physics.remap(states,action=action2)
 
-    adv = value_fun_eval.evaluate(next_states1) - value_fun_eval.evaluate(next_states2)
+    adv = value_fun_eval.evaluate(next_states1)\
+          - value_fun_eval.evaluate(next_states2)
     AdvImg = np.reshape(adv, (x_n,v_n))
     x_mesh = np.reshape(states[:,0], (x_n,v_n))
     v_mesh = np.reshape(states[:,1], (x_n,v_n))
 
-    three_d = False
-    if three_d:
-        fig = plt.figure()
-        ax = fig.gca(projection='3d')
-        ax.plot_surface(x_mesh,v_mesh,AdvImg,rstride=1, cstride=1, cmap=cm.jet, linewidth=0, antialiased=False)
-    else:
-        plt.pcolor(x_mesh,v_mesh,AdvImg)
+    plt.pcolor(x_mesh,v_mesh,AdvImg)
         
     plt.xlabel('Position')
     plt.ylabel('Velocity')
@@ -183,20 +177,20 @@ def generate_discretizer(x_desc,v_desc,action_desc,**kwargs):
     discount = kwargs.get('discount',0.99)
     assert(0 < discount < 1)
 
-    basic_mapper = InterpolatedRegularGridNodeMapper(x_desc,v_desc)
-    physics = DoubleIntegratorRemapper()
-    #cost_obj = QuadraticCost(cost_coef,set_point,override=oob_cost)
-    cost_obj = BallCost(np.array([0,0]),0.25)
+    basic_mapper = mdp.InterpolatedRegularGridNodeMapper(x_desc,v_desc)
+    physics = di.DoubleIntegratorRemapper()
+    #cost_obj = mdp.QuadraticCost(cost_coef,set_point,override=oob_cost)
+    cost_obj = mdp.BallCost(np.array([0,0]),0.25)
     actions = np.linspace(*action_desc)
 
     (x_lo,x_hi,x_n) = x_desc
     (v_lo,v_hi,v_n) = v_desc
     
-    left_oob_mapper = OOBSinkNodeMapper(xid,-float('inf'),x_lo,basic_mapper.num_nodes)
-    right_oob_mapper = OOBSinkNodeMapper(xid,x_hi,float('inf'),basic_mapper.num_nodes+1)
-    state_remapper = RangeThreshStateRemapper(vid,v_lo,v_hi)
+    left_oob_mapper = mdp.OOBSinkNodeMapper(xid,-float('inf'),x_lo,basic_mapper.num_nodes)
+    right_oob_mapper = mdp.OOBSinkNodeMapper(xid,x_hi,float('inf'),basic_mapper.num_nodes+1)
+    state_remapper = mdp.RangeThreshStateRemapper(vid,v_lo,v_hi)
 
-    discretizer = ContinuousMDPDiscretizer(physics,basic_mapper,cost_obj,actions)
+    discretizer = mdp.ContinuousMDPDiscretizer(physics,basic_mapper,cost_obj,actions)
     discretizer.add_state_remapper(state_remapper)
     discretizer.add_node_mapper(left_oob_mapper)
     discretizer.add_node_mapper(right_oob_mapper)
@@ -208,51 +202,67 @@ def generate_discretizer(x_desc,v_desc,action_desc,**kwargs):
 ###########################################
 # Generate VALUE FUNCTION
     
-def generate_value_function(discretizer,**kwargs):
+def find_value_function(discretizer,**kwargs):
 
     discount = kwargs.get('discount',0.99)
     max_iter = kwargs.get('max_iter',1000)
     thresh = kwargs.get('thresh',1e-6)
+    method = kwargs.get('method','value')
     
     mdp_obj = discretizer.build_mdp(discount=discount)
-    
-    method = 'kojima'
-    
+
+    # Build the LCP object
     if method in ['kojima']:
         print 'Building LCP object...'
         start = time.time()
         lcp_obj = mdp_obj.tolcp()
+        print 'Done. ({0:.2f}s)'.format(time.time() - start)
+
+    # Build the Projective LCP object
+    if method in ['projective']:
+        print 'Building projected LCP object...'
+        start = time.time()
+        lcp_obj = mdp_obj.tolcp()
         print 'Done. ({0:.2f}s)'.format(time.time() - start)        
-    
+
+    # Select the iterator
     if method == 'value':
         iter = ValueIterator(mdp_obj)
     elif method == 'kojima':
         iter = KojimaIPIterator(lcp_obj)
-    
+    else:
+        raise NotImplmentedError()
+
+    # Set up the solver object
     solver = solvers.IterativeSolver(iter)
-    solver.termination_conditions.append(MaxIterTerminationCondition(max_iter))
+    max_iter_cond = MaxIterTerminationCondition(max_iter)
+    solver.termination_conditions.append(max_iter_cond)
     if method in ['value']:
-        solver.termination_conditions.append(ValueChangeTerminationCondition(thresh))
-        solver.notifications.append(ValueChangeAnnounce())
-        
+        val_change_term = ValueChangeTerminationCondition(thresh)
+        solver.termination_conditions.append(val_change_term)
+        solver.notifications.append(ValueChangeAnnounce())        
     elif method in ['kojima']:
-        solver.termination_conditions.append(ResidualTerminationCondition(thresh))
+        res_change_term = ResidualTerminationCondition(thresh)
+        solver.termination_conditions.append(res_change_term)
         solver.notifications.append(ResidualChangeAnnounce())
 
 
+    # Actually do the solve
     print 'Starting {0} solve...'.format(type(iter))
     start = time.time()
     solver.solve()
     print 'Done. ({0:.2f}s)'.format(time.time() - start)
 
+    # Extract the cost-to-go function
     if method == 'value':
         J = iter.get_value_vector()
     elif method == 'kojima':
         N = mdp_obj.num_states
         J = iter.get_primal_vector()[:N]
-        
-    value_fun_eval = BasicValueFunctionEvaluator(discretizer,J)
-    
+
+    # Place in an interpolating evaluator
+    value_fun_eval = mdp.BasicValueFunctionEvaluator(discretizer,J)
+    # TODO: use the low-dimension weights and basis if projective
     return value_fun_eval
 
 
@@ -282,6 +292,6 @@ if __name__ == '__main__':
     #plot_value_function(discretizer,value_fun_eval)
     #plot_advantage(discretizer,value_fun_eval,-1,1)
     lookahead = 3
-    policy = KStepLookaheadPolicy(discretizer, value_fun_eval, discount,lookahead)
+    policy = mdp.KStepLookaheadPolicy(discretizer, value_fun_eval, discount,lookahead)
     plot_policy(discretizer,policy)
     #plot_trajectory(discretizer,policy)
