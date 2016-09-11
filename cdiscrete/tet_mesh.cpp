960c09bd96ab67d91af002f59490139b2c271568
Erik Zawadzki <epz@cs.cmu.edu>
2016-09-11 17:44:55 -0400

    Working on figuring out how to do 3d tetrahedral stuff.

--- cdiscrete/tet_mesh.cpp
+++ cdiscrete/tet_mesh.cpp
@@ -1,63 +1,56 @@
-#include <iostream>
-
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Simple_cartesian.h>
 
 #include <CGAL/Mesh_triangulation_3.h>
 #include <CGAL/Mesh_complex_3_in_triangulation_3.h>
 #include <CGAL/Mesh_criteria_3.h>
 
 #include <CGAL/Polyhedral_mesh_domain_3.h>
-
+#include <CGAL/refine_mesh_3.h>
 #include <CGAL/make_mesh_3.h>
+
 #include <CGAL/convex_hull_3.h>
 
-#include <armadillo>
-
-
+#include <vector>
+#include <iostream>
+#include <fstream>
+// IO
+#include <CGAL/IO/Triangulation_geomview_ostream_3.h>
 
 // Domain
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-//typedef CGAL::Simple_cartesian<double> K;
 typedef K::Point_3 Point;
-
 typedef CGAL::Polyhedron_3<K> Polyhedron;
-typedef CGAL::Polyhedral_mesh_domain_3<Polyhedron,K> Mesh_domain;
+typedef CGAL::Polyhedral_mesh_domain_3<Polyhedron, K> Mesh_domain;
 
 // Triangulation
-#ifdef CGAL_CONCURRENT_MESH_3
-  typedef CGAL::Mesh_triangulation_3<
-    Mesh_domain,
-    CGAL::Kernel_traits<Mesh_domain>::Kernel, // Same as sequential
-    CGAL::Parallel_tag                        // Tag to activate parallelism
-  >::type Tr;
-#else
-  typedef CGAL::Mesh_triangulation_3<Mesh_domain>::type Tr;
-#endif
-
+typedef CGAL::Mesh_triangulation_3<Mesh_domain>::type Tr;
 typedef CGAL::Mesh_complex_3_in_triangulation_3<Tr> C3t3;
 // Criteria
 typedef CGAL::Mesh_criteria_3<Tr> Mesh_criteria;
 // To avoid verbose function and named parameters call
 using namespace CGAL::parameters;
-using namespace arma;
-void build_rect(const vec & lb,
-                const vec & ub,
+
+/*
+  Function for building a rectangular prism.
+  This 
+*/
+void build_rect(const std::vector<double> & lb,
+                const std::vector<double> ub,
                 Polyhedron & poly){
   
-  std::vector<Point> points;
-  vec p = vec(3);
+std::vector<Point> points;
+  double p[3];
   Point P;
   for(uint b = 0; b < pow(2,3); b++){
     for(uint i = 0; i < 3; i++){
       if(b & (1 << i)){
-        p(i) = ub(i);
+        p[i] = ub[i];
       }
       else{
-        p(i) = lb(i);
+        p[i] = lb[i];
       }
     }
-    P = Point(p(0),p(1),p(2));
+    P = Point(p[0],p[1],p[2]);
     points.push_back(P);
   }
   CGAL::convex_hull_3(points.begin(), points.end(), poly);  
@@ -65,29 +58,33 @@ void build_rect(const vec & lb,
 
 int main()
 {
-  Polyhedron poly1;
-  vec lb = -1 * ones<vec>(3);
-  vec ub = 1 * ones<vec>(3);
-  build_rect(lb,ub,poly1);
-
-  Polyhedron poly2;
-  lb = zeros<vec>(3);
-  ub = 2 * ones<vec>(3);
-  build_rect(lb,ub,poly2);
-
+  std::vector<double> lb;
+  std::vector<double> ub;
+  for(uint d = 0; d < 3; d++){
+    lb.push_back(-1.0);
+    ub.push_back(1.0);
+  }
+  Polyhedron poly;
+  build_rect(lb,ub,poly);
   
-  // Domain (Warning: Sphere_3 constructor uses squared radius !)
-  Mesh_domain domain(poly1);
-  // Mesh criteria
-  Mesh_criteria criteria(facet_angle=30, facet_size=0.15, facet_distance=0.025,
-                         cell_radius_edge_ratio=2, cell_size=0.15);  
+  // Define functions
+  // Domain (Warning: Sphere_3 constructor uses square radius !)
+  Mesh_domain domain(poly);
+  // Set mesh criteria
+  Mesh_criteria criteria(facet_angle=25, facet_size=0.15, facet_distance=1e-2,
+                         cell_radius_edge_ratio=3);
   // Mesh generation
   C3t3 c3t3 = CGAL::make_mesh_3<C3t3>(domain, criteria);
-  std::cout << "Number of cells: " << c3t3.number_of_cells() << std::endl;
-  std::cout << "Number of facets: " << c3t3.number_of_facets() << std::endl;
-
-  // Output
+  
+  // Mesh output
   std::ofstream medit_file("out.mesh");
   c3t3.output_to_medit(medit_file);
+
+  // Triangulation output and input
+  std::cout << c3t3.triangulation();
+  C3t3 c3t3_read_back;
+  std::cin >> c3t3_read_back.triangulation();
+  //NB: file compiles when these last three lines are commented out.
+ 
   return 0;
 }
