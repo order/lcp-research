a9ca5275dd154ceca8ae72eafc3dd100048c2adf
Erik Zawadzki <epz@cs.cmu.edu>
2016-10-25 15:45:31 -0400

    Putting a pin in work on Mangasarian's mu

--- foo.py
+++ foo.py
@@ -1,31 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from utils.archiver import *
-from scipy.stats import pearsonr
 
-import tri_mesh_viewer as tmv
+data = np.load("test.npz")
+mu = data['mu']
+d = data['dims']
 
-def iqr(x,Y,marker):
-    plt.gca()
-    plt.plot(x,np.median(Y,0),'-' + marker,lw=2)
-    plt.plot(x,np.percentile(Y,25,axis=0),'--' + marker,lw=2)
-    plt.plot(x,np.percentile(Y,75,axis=0),'--' + marker,lw=2)
-
-
-dir = '/home/epz/data/minop/'
-files = [dir + 'flow_refine_0_false.exp_res',
-         dir + 'flow_refine_10_false.exp_res',
-         dir + 'flow_refine_15_false.exp_res']
-colors = ['b','r','g']
 
 plt.figure()
-for (C,F) in zip(colors,files):
-    unarch = Unarchiver(F)
-    nb = unarch.num_basis[:-1]
-    res = unarch.residuals
-    (I,R) = res.shape
+plt.plot(d,mu,'.-')
+plt.xlabel('size')
+plt.ylabel('mu')
+plt.title('l1-norm mu')
 
-    for i in xrange(I):
-        plt.plot(nb, res[i,:], C+'-',alpha=0.15)    
-    iqr(nb,res,C)
+plt.figure()
+plt.plot(np.diff(mu),'o-')
+plt.ylabel('mu difference')
+plt.title('l1-norm mu difference')
+
 plt.show()
--- mangasarian_mu.py
+++ mangasarian_mu.py
@@ -1,19 +1,17 @@
 import numpy as np
 import scipy.sparse as sps
+import matplotlib.pyplot as plt
+
 import itertools
 from pulp import *
 
 import time
 from collections import deque
 
-def brute_force_dependent_sets_from_frame(F):
-    (n,N) = F.shape
-    
-    Indices = []
-    for idx in itertools.combinations(range(N),n):
-        if n != np.linalg.matrix_rank(F[:,idx]):
-            Indices.append(idx)
-    return Indices
+def lin_dep(F,cols):
+    k = len(cols)
+    rank = np.linalg.matrix_rank(F[:,cols])
+    return rank < k
 
 def minimal_dependent_set_from_frame(F):
     return help_dep(F,0,set([]))
@@ -27,7 +25,7 @@ def help_dep(F,pos,cols):
     
     assert k <= n
     
-    if k > 1 and np.linalg.matrix_rank(F[:, list(cols)]) < k:
+    if k > 1 and lin_dep(F,cols):
         return [tuple(cols)] # Any extension also dependent
 
     if k == n:
@@ -62,7 +60,7 @@ def dependent_set_from_frame(F):
         if eclipsed:
             continue
 
-        if np.linalg.matrix_rank(F[:,list(cand)]) < len(cand):
+        if lin_dep(F,cand):
             sets.append(scand)
             continue
 
@@ -74,9 +72,31 @@ def dependent_set_from_frame(F):
 
     return sets
                    
+def dep_sets_from_identity(F):
+    (n,N) = F.shape
+    assert np.linalg.norm(F[:,-n:] - np.eye(n)) < 1e-15
+
+    dep_sets = []
+
+    for i in xrange(N-n):
+        idx = np.where(F[:,i] != 0)[0]
+        if len(idx) >= (n/2):
+            continue
+        new_set = [i] + [j+n+1 for j in idx]
+        assert lin_dep(F,new_set)
+        dep_sets.append(tuple(new_set))
+    return dep_sets
+
+def dep_sets_from_combination(F,k):
+    (n,N) = F.shape
     
+    Indices = []
+    for idx in itertools.combinations(range(N),k):
+        if lin_dep(F,idx):
+            Indices.append(idx)
+    return Indices   
   
-def mangasarian_mu(F,dep_sets,linf_comp):
+def mangasarian_mu(F,dep_sets,linf_comp=None):
     """
     Solve for the theoretical perturbation constant in
     "Error bounds for non-degenerate monotone LCPs" by
@@ -86,7 +106,7 @@ def mangasarian_mu(F,dep_sets,linf_comp):
     prob = LpProblem("Mangasarian Mu problem",LpMaximize)
     (n,N) = F.shape
     
-    U = 1e9-1 # Magnitude bound
+    U = 1e3 # Magnitude bound
 
     # Variables and their support twiddles
     # So (T[i] = 0) -> (X[i] = 0)
@@ -116,55 +136,148 @@ def mangasarian_mu(F,dep_sets,linf_comp):
         
         prob += (LpAffineExpression(coef_list) <= AV[i]), "AbsVal_{0}_{1}".format(i,0)
         prob += (-AV[i] <= LpAffineExpression(coef_list)), "AbsVal_{0}_{1}".format(i,1)
+        
     prob += (LpAffineExpression(zip(AV,[1]*n))) <= 1, "l1 norm"
 
-    prob += (LpAffineExpression(zip(T,[1]*N))) <= n, "toggle"
+    prob += (LpAffineExpression(zip(T,[1]*N))) == n, "toggle"
     for (C,s) in enumerate(dep_sets):
         prob += (LpAffineExpression([(T[i],1) for i in s]) <= len(s)-1), "dep_set_"+str(C)
-
-    #print prob
         
     prob.solve()
-    #print "\tStatus:", LpStatus[prob.status]
     #print "\tMangasarian mu:", value(prob.objective)
+    #print "Status:", LpStatus[prob.status]
 
-    return value(prob.objective)
+    x_array = np.empty(N)
+    for (i,x) in enumerate(X):
+        x_array[i] = x.varValue
+    av_array = np.empty(n)
+    for (i,av) in enumerate(AV):
+        av_array[i] = av.varValue
+    t_array = np.empty(N)
+    for (i,t) in enumerate(T):
+        t_array[i] = t.varValue
+    
+    return value(prob.objective),x_array
 
+def build_hallway(N,g,p):
+    
+        P = sps.spdiags((1-p)*np.ones(N),0,N,N) + sps.spdiags(p*np.ones(N),-1,N,N)
+        P = P.tolil()
+        P[0,-1] = p
+
+        Q = sps.spdiags((1-p)*np.ones(N),0,N,N) + sps.spdiags(p*np.ones(N),1,N,N)
+        Q = Q.tolil()
+        Q[-1,0] = p
+        
+        I = sps.eye(N)
+        B = I - g * P
+        C = I - g*Q
+        M = sps.bmat([[None,B,C],
+                      [-B,None,None],
+                      [-C,None,None]])
+
+        c = np.ones(N)
+        c[int(N/2)] = 0
+        
+        q = np.empty(3*N)
+        q[:D] = -np.ones(N) / N
+        q[D:(2*D)] = c
+        q[(2*D):] = c
+
+        return (M,q)
+
+def build_smallway(N,g,p):    
+        P = sps.spdiags((1-p)*np.ones(N),0,N,N) + sps.spdiags(p*np.ones(N),-1,N,N)
+        P = P.tolil()
+        P[0,-1] = p
+        
+        I = sps.eye(N)
+        B = I - g * P
+        M = sps.bmat([[None,B],
+                      [-B,None]])
+
+        c = np.ones(N)
+        c[int(N/2)] = 0
+        
+        q = np.empty(2*N)
+        q[:D] = -np.ones(N) / N
+        q[D:] = c
+
+        return (M,q)
+
+def check_solution(F,x):
+    idx = list(np.where(x > 0)[0])
+    if not lin_dep(F,idx):
+        return True
+
+    # Refine
+    repeat = True
+    while repeat:
+        repeat = False
+        for i in xrange(len(idx)):
+            cand = idx[:i] + idx[(i+1):] # Everything other than i
+            
+            if lin_dep(F,cand):
+                repeat = True
+                idx = cand
+                break
+    return idx
+
+def constraint_gen(F,dep_sets,i):
+    I = 0
+    while True:
+        I+=1
+        #print "\tCGR:",I
+        obj,x = mangasarian_mu(F,dep_sets,i)
+        res = check_solution(F,x)
+        if res is True:
+            return obj
+        else:
+            assert len(res) <= n
+            dep_sets.append(res)
+
+def linf_mu(F,dep_sets):
+    (n,N) = F.shape
+    linf = -np.inf
+    for i in xrange(N):
+        #print "Component ",i
+        obj = constraint_gen(F,dep_sets,i)
+        linf = max(linf,obj)
+    return linf
+
+def l1_mu(F,dep_sets):
+    return constraint_gen(F,dep_sets,None)
     
 if __name__ == "__main__":
-    for D in xrange(2,10):
-        # Form MC evaluation LCP (A=1)
-        print "D:",D
-        g = 0.98
-        P = sps.spdiags(0.25*np.ones(D),0,D,D) + sps.spdiags(0.75*np.ones(D),-1,D,D)
-        P = P.tolil()
-        P[0,-1] = 0.75
-        
-        B = sps.eye(D) - g * P
-        M = sps.bmat([[None,B],[-B.T,None]])
-        q = np.empty(2*D)
-        q[:D] = -np.ones(D) / D
-        q[D:] = np.ones(D)
-        q[-1] = 0
 
+    dims = []
+    mu = []
+    for D in range(2,15):
+        # Form MC evaluation LCP (A=1)
         n = 2*D
+        (M,q) = build_smallway(D,0.99,0.75)
+        assert (n,n) == M.shape
         N = 2*n+1
 
         # Build the frame [-q,-M,I]
         F = sps.hstack([-np.reshape(q,(n,1)),-M,sps.eye(n)]).toarray()
         assert (n,N) == F.shape
 
-        # Find all minimal dependent sets (expensive!)
+        # Find dep sets using the identity matrix
         t2 = time.time()
-        dep_sets = dependent_set_from_frame(F)
+        dep_sets = []
+        #dep_sets = dep_sets_from_identity(F)
+        #dep_sets += dep_sets_from_combination(F,2)
+        dep_sets += dep_sets_from_combination(F,3)
+        #dep_sets += dep_sets_from_combination(F,4)
         t3 = time.time()
 
         #print "Found",len(dep_sets),"dependent sets in", (t3 - t2), "seconds"
-
-        linf_obj = -np.inf
-        for i in xrange(N):
-            #print "\tComponent LP for",i
-            obj = mangasarian_mu(F,dep_sets,i)
-            linf_obj = max(linf_obj,obj)
-
-        print "Linf:",linf_obj
+        #print "\tComponent LP for",i
+        obj = linf_mu(F,dep_sets)
+        #obj = l1_mu(F,dep_sets)
+        
+        dims.append(n)
+        mu.append(obj)
+        print "mu("+str(n)+") = ",obj
+        np.savez('test',dims=dims,mu=mu)
